d446b9ea4fa85b34081581200bff59c3e58e4091
atul.kumar1 <atul.kumar1@cgi.com>
2019-08-27 17:17:17 +0530

    Merge branch 'master' of /home/cgi/IdeaProjects/Currently Working/text-cleansing with conflicts.

--- src/main/java/com/stackroute/succour/TextCleansingApplication.java
+++ src/main/java/com/stackroute/succour/TextCleansingApplication.java
@@ -1,26 +1,13 @@
 package com.stackroute.succour;
 
-import com.stackroute.succour.nonalphanumericremover.RemoveSpecialCharacter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
-
-import java.io.IOException;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.stackroute.succour")
-@Import(RemoveSpecialCharacter.class)
-
 public class TextCleansingApplication {
 
-	public static void main(String[] args) throws IOException {
-
+	public static void main(String[] args) {
 		SpringApplication.run(TextCleansingApplication.class, args);
-		System.out.println(RemoveSpecialCharacter.result());
-
-
-
 	}
 
 }
