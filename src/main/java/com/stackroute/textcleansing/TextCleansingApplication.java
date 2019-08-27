6ed92d9e5c1d7b309340e21cfc4d1407d325f893
samareshmaiti <samaresh02maiti@gmail.com>
2019-08-27 12:01:55 +0530

    started work with text-cleansing

---
+++ src/main/java/com/stackroute/textcleansing/TextCleansingApplication.java
@@ -0,0 +1,13 @@
+package com.stackroute.textcleansing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextCleansingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextCleansingApplication.class, args);
+	}
+
+}
